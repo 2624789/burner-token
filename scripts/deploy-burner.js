c1ab7b4b283702e3413c87ed355726f81d32f7da
2624789 <2624789@users.noreply.github.com>
2022-06-11 09:19:58 -0500

    Add contracts to frontend

--- scripts/deploy-burner.js
+++ scripts/deploy-burner.js
@@ -1,17 +1,45 @@
  // scripts/deploy-burner.js
 
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 // 10000 * 1e18
 const INITIAL_SUPPLY = '10000000000000000000000';
 
 async function main() {
-  const Burner = await hre.ethers.getContractFactory("Burner");
+  const Burner = await hre.ethers.getContractFactory('Burner');
   console.log('Deploying Burner...');
   const burner = await Burner.deploy(INITIAL_SUPPLY);
 
   await burner.deployed();
-  console.log("Burner deployed to:", burner.address);
+  console.log('Burner deployed to:', burner.address);
+
+  // Save contract's artifacts and address in the frontend directory
+  saveFrontendFiles(burner);
+}
+
+function saveFrontendFiles(burner) {
+  console.log('Saving frontend files...');
+  const fs = require('fs');
+  const contractsDir = __dirname + '/../frontend/src/contracts';
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + '/contract-address.json',
+    JSON.stringify({
+      Burner:  burner.address,
+    }, undefined, 2)
+  );
+
+  // Burner
+  const BurnerArtifact = artifacts.readArtifactSync('Burner');
+
+  fs.writeFileSync(
+    contractsDir + '/Burner.json',
+    JSON.stringify(BurnerArtifact, null, 2)
+  );
 }
 
 main()
